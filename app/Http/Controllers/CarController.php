b263ae73e9210422ea650023582ce0de6628af72
NanyHuna <nany.kuzola@zap.co.ao>
2024-09-21 16:09:41 -0300

    feature/onCarApp-01: api configuration(route, controller, requests and endpoints), creation of Cars table/migration and seeds

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Http\Request;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class CarController extends Controller
+{
+    //
+
+
+}
---
+++ app/Http/Requests/CarRequest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CarRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     *
+     * Validate input requests
+     *
+
+     * @param Validator $validator
+     * @return array
+     * @since release of feature/onCarApp-01
+     */
+    public function failedValidation(Validator $validator): array
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => false,
+            $validator->errors()
+        ],422
+        ));
+    }
+
+    /**
+     *
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     *@since release of feature/onCarApp-01
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'modelo' => 'required|string|min:3',
+            'marca' => 'required|string|min:3',
+            'cor' => 'required|string|min:4',
+        ];
+    }
+
+
+    /**
+     *
+     * return optimized messages of request validations
+     *
+     * @return string[]
+     * @since release of feature/onCarApp-01
+     */
+    public function messages(): array
+    {
+        return [
+            'modelo.required' => 'You need to set model name.',
+            'modelo.string' => 'it must be a string',
+            'modelo.min'=>  'you need to set model with at least 3 characters.',
+            'marca.required' => 'you need to set marca.',
+            'marca.string' => 'it must be a string.',
+            'marca.min' => 'it must have at least 4 letters.',
+            'cor.required' => 'you need to set cor.',
+            'cor.string' => 'it must be a string',
+            'cor.min' => 'must have at least 4 characters.',
+
+        ];
+    }
+}
